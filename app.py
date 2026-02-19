70973434e99533c788bc6e7958f84c9386c2cbbc
vivek sah <viveksha129@gmail.com>
2026-02-20 03:09:38 +0530

    Adjust margin-top for main title in app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ with col_title:
             font-size: 64px;
             font-weight: 900;
             color: white;
-            margin-top: -5px;
+            margin-top: -10px;
         }
         </style>
 
